53dad8defa477132ff7d5bfca7842f737fc1d323
Piotr Kranendonk <piotrdaniel@hotmail.nl>
2026-02-26 21:55:48 +0100

    Complete DataAnalysis.py and add instrucions to README.md

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -2,18 +2,33 @@ import numpy as np
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
+import scipy as sp
 
 
 # Get all important data from the csv's
 class Measurement:
     def __init__(self, filename):
-        
         data = pd.read_csv(filename)
         
-        self.name = filename.rstrip('.csv').replace('/', ' ').replace('results_', '')
-        self.delta_J = data['PACKAGE_ENERGY (J)'].diff()[2:-1].to_numpy()
+        self.name = filename
         self.delta_t = data['Delta'][2:-1].to_numpy()/1000
-        self.watt = self.delta_J / self.delta_t
+        
+        energy_key = "PACKAGE_ENERGY (J)"
+        if "CPU_ENERGY (J)" in data.columns:
+            energy_key  = "CPU_ENERGY (J)"
+        elif "PACKAGE_ENERGY (W)" in data.columns:
+            energy_key = "PACKAGE_ENERGY (W)"
+        elif "SYSTEM_POWER (Watts)" in data.columns:
+            energy_key = "SYSTEM_POWER (Watts)"
+        elif "CPU_POWER (Watts)" in data.columns:
+            energy_key = "CPU_POWER (Watts)"
+
+        if energy_key == "PACKAGE_ENERGY (J)" or energy_key == "CPU_ENERGY (J)":
+            self.delta_J = data[energy_key].diff()[2:-1].to_numpy()
+            self.watts = self.delta_J / self.delta_t
+        else:
+            self.watts = data[energy_key][2:-1].to_numpy()
+            self.delta_J = self.watts * self.delta_t
 
         self.total_J = np.sum(self.delta_J)
         self.total_t = np.sum(self.delta_t)
@@ -40,35 +55,65 @@ class Database:
         ax.violinplot(Watt_data, positions=[1], showextrema=False)
         
         ax.legend()
-        ax.set_ylabel('Energie consumption [Watt]')
-        ax.set_title('Energy measurements null setting')
+        ax.set_ylabel('Power consumption [Watt]')
+        ax.set_title('Power measurements null setting')
     
-    def plot_threads(self, ax):
+    def plot_threads_joule(self, ax):
         null_watts = np.mean([m.avg_W for m in self.null_measurements])
         
         for i, (thread_type, thread_measurements) in enumerate(self.measurements.items()):
             thread_data = np.array([m.total_J - m.total_t*null_watts for m in thread_measurements])
             thread_amount = thread_type.split('_')[-1]
             
-            ax.boxplot(thread_data, positions=[i], tick_labels=[thread_amount], label=f'{thread_amount} threads: {round(np.mean(thread_data),3)} J')
+            ax.boxplot(thread_data, positions=[i], tick_labels=[thread_amount], label=f'{thread_amount} threads: {round(np.mean(thread_data),1)} J')
             ax.violinplot(thread_data, positions=[i], showextrema=False)
         
         ax.legend(title='Mean consumption')
-        ax.set_ylabel('Energie consumption [J]')
+        ax.set_ylabel('Energy consumption [J]')
         ax.set_xlabel('Number of threads')
-        ax.set_title('Energie measurements varying thread amount')
+        ax.set_title('Energy measurements varying thread amount')
+    
+    def plot_threads_watts(self, ax):
+        null_watts = np.mean([m.avg_W for m in self.null_measurements])
+        
+        for i, (thread_type, thread_measurements) in enumerate(self.measurements.items()):
+            thread_data = np.array([m.avg_W - null_watts for m in thread_measurements])
+            thread_amount = thread_type.split('_')[-1]
+            
+            ax.boxplot(thread_data, positions=[i], tick_labels=[thread_amount], label=f'{thread_amount} threads: {round(np.mean(thread_data),1)} Watt')
+            ax.violinplot(thread_data, positions=[i], showextrema=False)
+        
+        ax.legend(title='Mean consumption')
+        ax.set_ylabel('Power consumption [Watt]')
+        ax.set_xlabel('Number of threads')
+        ax.set_title('Power measurements varying thread amount')
 
 
 def main():
-    null_folder = 'null_measurements'
-    thread_folders = ['threads_1', 'threads_5', 'threads_10']
+    null_folder = 'null'
+    thread_folders = glob.glob(f'threads_*')
 
     analysis = Database(null_folder, thread_folders)
 
-    fig, ax = plt.subplots(1,2, figsize=(12,5))
+    null_watts = np.mean([m.avg_W for m in analysis.null_measurements])
+
+    print("Testing if collected data follows normal distribution with Shapiro")
+    for folder in [null_folder] + thread_folders:
+        if folder == null_folder:
+            folder_data = [m.total_J - m.total_t*null_watts for m in analysis.null_measurements]
+        else:
+            folder_data = [m.total_J - m.total_t*null_watts for m in analysis.measurements[folder]]
+        _, shapiro_p = sp.stats.shapiro(folder_data)
+        if shapiro_p >= 0.05:
+            print(f'{folder}: Data is normal \np_value = {round(shapiro_p,5)}\n')
+        else:
+            print(f'{folder}: Data is NOT normal \np_value = {round(shapiro_p,5)}\n')
+
+    fig, ax = plt.subplots(1,3, figsize=(12,4))
 
     analysis.plot_null(ax[0])
-    analysis.plot_threads(ax[1])
+    analysis.plot_threads_joule(ax[1])
+    analysis.plot_threads_watts(ax[2])
 
     plt.show()
 
--- README.md
+++ README.md
@@ -40,3 +40,33 @@ For example:
 ```shell
 scripts/program_measurement.ps1 SINGLE_THREADED 100
 ```
+
+Varying thread amounts will lead to different running times. To make sure the analysis will be as effective as possible you should also make null measurements of your devices energy consumption when it is doing nothing. To do this you can run the following:
+```shell
+scripts/null_measurement.ps1
+```
+
+## How to analyse data
+
+After running measurements for 1 set of arguments, you will find 30 csv files containing the results in your repository. Create a directory in this repository with a name corresponding to the type of measurement and move all the csv files into it. See required directory names:
+
+| Measurement type            | Directory name      |
+|-----------------------------|---------------------|
+| null measurement            | `null`              |
+| measurement with n threads  | `threads_<n>`       |
+
+So a directory with measurements for an experiment with 4 threads should be called `threads_4`
+
+When you have done this you can run a python script:
+```shell
+python DataAnalysis.py
+```
+
+This will show:
+- some plots of the acquired data
+    - Average power (W) consumption of the null measurements
+    - Total energy (J) consumption of the thread measurements
+    - Average power (W) consumption of the thread measurements
+- Shapiro p_values for each measurement that show if the data follows a normal distribution. 
+
+If a measurement is not a normal distribution there might have been some issues with your testing set up. 
